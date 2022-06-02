bd7d99bee679d9b929493496a885069994deec07
Aniket <aniketsaste96@gmail.com>
2022-06-02 21:01:27 +0530

    validation

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -12,6 +12,19 @@ exports.register = async (req, res) => {
             return res.status(400).json({ message: "Invalid Email" })
         }
 
+        //check if email is already exits or not
+        const check = await User.findOne({ email });
+        if (check) {
+            return res.status(400).json({ message: "Email Already exists,Try with different email address" })
+        }
+
+        //check if users age is more than 12 years
+        if ((2022 - bYear) < 12) {
+            return res.status(400).json({ message: "Minimum age to register is 12,Sorry You are not allowed" })
+        }
+
+        //password hash
+
         const user = await new User({
             //can also use req.body
             first_name, last_name, email, username, bYear, bMonth, bDay, password, gender
